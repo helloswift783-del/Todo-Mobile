afcdd637ccc6f1b5fdad5f007dcfaf15852cee03
helloswift783-del <helloswift783@gmail.com>
2026-03-01 09:19:14 +0530

    Create gradient_background.dart

---
+++ lib/views/widgets/gradient_background.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class GradientBackground extends StatelessWidget {
+  const GradientBackground({
+    required this.child,
+    super.key,
+  });
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    final scheme = Theme.of(context).colorScheme;
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [
+            scheme.primaryContainer,
+            scheme.surface,
+            scheme.secondaryContainer,
+          ],
+        ),
+      ),
+      child: child,
+    );
+  }
+}
